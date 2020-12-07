862de33b9b28c021217182e368764e8608b00de3
Brian-Kraosio <briansayudha@gmail.com>
2020-12-07 08:46:15 +0700

    Tab View Fixed

--- app/src/main/java/id/putraprima/mygoldtracker/adapter/TabGoldAdapter.java
+++ app/src/main/java/id/putraprima/mygoldtracker/adapter/TabGoldAdapter.java
@@ -7,7 +7,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
+import id.putraprima.mygoldtracker.screen.buy.BuyFragment;
 import id.putraprima.mygoldtracker.screen.portolio.PorfolioFragment;
+import id.putraprima.mygoldtracker.screen.transactions.TransactionsFragment;
+import id.putraprima.mygoldtracker.screen.wallet.WalletFragment;
 
 public class TabGoldAdapter extends FragmentPagerAdapter {
 
@@ -26,7 +29,18 @@ public class TabGoldAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position){
-        return PorfolioFragment.newInstance(position+1);
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new WalletFragment();
+                break;
+            case 1:
+                fragment = new BuyFragment();
+                break;
+            case 2 :
+                fragment = new TransactionsFragment();
+        }
+        return fragment;
     }
 
     @Override public CharSequence getPageTitle(int position){
--- app/src/main/java/id/putraprima/mygoldtracker/screen/portolio/PorfolioFragment.java
+++ app/src/main/java/id/putraprima/mygoldtracker/screen/portolio/PorfolioFragment.java
@@ -34,7 +34,7 @@ public class PorfolioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_porfolio,container,false);
         TextView textView = (TextView) view;
-        textView.setText("Fragment #"+mPage);
+        textView.setText("blablabla " + mPage);
         return view;
     }
 }
